abc5b4929cbb3f6ccc97e4d146bc095619e9bf91
zjor <zjor.se@gmail.com>
2016-05-09 00:01:50 +0200

    missing constructor

--- src/main/java/com/github/zjor/telegram/bot/api/dto/ReplyKeyboardMarkup.java
+++ src/main/java/com/github/zjor/telegram/bot/api/dto/ReplyKeyboardMarkup.java
@@ -1,10 +1,12 @@
 package com.github.zjor.telegram.bot.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReplyKeyboardMarkup {
 
     /**
